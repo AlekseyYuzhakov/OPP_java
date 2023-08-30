5bfd9b7e28991159d412355f5a67b9c4cec4367c
Aleksey Yuzhakov <theval4a@gmail.com>
2023-08-30 07:48:35 +0500

    homeWork1 change

--- homeWork1/task1/Owner.java
+++ homeWork1/task1/Owner.java
@@ -1,16 +1,20 @@
 package homeWork1.task1;
 
+//Обьявлен класс Owner
 public class Owner {
   private String name;
 
+  // Геттер для класса Owner
   public String getName() {
     return name;
   }
 
+  // Сеттер для класса Owner
   public void setName(String name) {
     this.name = name;
   }
 
+  // Функция приветствия
   public void greet() {
     System.out.println("Привет меня зовут " + name);
   }
--- homeWork1/task2/Rectangle.java
+++ homeWork1/task2/Rectangle.java
@@ -1,46 +1,56 @@
 package homeWork1.task2;
 
+// Обьявлен класс Rectangle
 public class Rectangle {
   private Integer width;
   private Integer height;
 
+  // Базовый конструктор для класса Rectangle
   public Rectangle(Integer width, Integer height) {
     this.width = width;
     this.height = height;
   }
 
+  // Конструктор для класса Rectangle одинаковые стороны
   public Rectangle(Integer equal) {
     this.height = equal;
     this.width = equal;
   }
 
+  // Пустой конструктор для класса Rectangle
   public Rectangle() {
     this.height = 1;
     this.width = 1;
   }
+  // Геттер для width
 
   public Integer getWidth() {
     return width;
   }
+  // Сеттер для width
 
   public void setWidth(Integer width) {
     System.out.println("Ширина = " + width);
     this.width = width;
   }
+  // Геттер для height
 
   public Integer getHeight() {
     return height;
   }
+  // Сеттер для height
 
   public void setHeight(Integer height) {
     System.out.println("Высота = " + height);
     this.height = height;
   }
+  // Функция для поиска площади
 
   public Integer calculateArea() {
     return height * width;
   }
 
+  // Функция для поиска периметра
   public Integer calculatePerimeter() {
     return (2 * (height + width));
   }
