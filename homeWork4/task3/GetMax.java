9d7dd71111736b6ab486a37200ef0a0f83ce0248
Aleksey Yuzhakov <theval4a@gmail.com>
2023-09-14 19:44:24 +0500

    homeWork5 added

--- homeWork4/task3/GetMax.java
+++ homeWork4/task3/GetMax.java
@@ -1,26 +1,26 @@
 package homeWork4.task3;
 
-public class GetMax<T> implements Comparable<T> {
-  private T o;
+// public class GetMax<T> implements Comparable<T> {
+//   private T o;
 
   // Как то очень сложно. 1.5 часа и гугл не смогли мне помочь в этом вопросе((
 
-  @Override
-  public int compareTo(T o) {
-    int result = this.o.compareTo(o);
-    return 0;
-  }
+  // @Override
+  // public int compareTo(T o) {
+  //   int result = this.o.compareTo(o);
+  //   return 0;
+  // }
 
-  @Override
-  public int icompareTo(GetMax<T> o1) {
-    return this.o.compareTo(o1);
-  }
+  // @Override
+  // public int icompareTo(GetMax<T> o1) {
+//   //   return this.o.compareTo(o1);
+//   // }
 
-  public static void main(String[] args) {
+//   public static void main(String[] args) {
 
-  }
+//   }
 
-}
+// }
 
 // Задача 3 (по желанию):Напишите обобщенную функцию getMax, которая принимает
 // два параметра типа T и возвращает максимальное значение из них.
---
+++ homeWork5/CalculatorView.java
@@ -0,0 +1,45 @@
+package homeWork5;
+
+import java.util.LinkedList;
+
+/**
+ * Класс вьюер для вывода информации в терминал
+ */
+public class CalculatorView {
+    /**
+     * Функция вывода результата в терминал
+     * @param result результат вычеслений для вывода на экран
+     */
+    public void printResult(double result) {
+        System.out.println("Результат: " + result);
+    }
+
+    /**
+     * Функция вывода ошибки в терминал
+     * @param errorMessage ошибка для вывода на эран
+     */
+    public void printError(String errorMessage) {
+        System.err.println("Ошибка: " + errorMessage);
+    }
+
+    /**
+     * Функция для вывода истории вычеслений
+     * @param list история вычеслений
+     */
+    public void printStoryLog(LinkedList<String> list) {
+        System.out.println("История вычеслений ");
+        for (String string : list) {
+            System.err.println(string);
+        }
+    }
+
+    /**
+     * Функция отображения меню
+     *  */ 
+    public void printMenu() {
+        System.out.println("1. Произвести вычесления ");
+        System.out.println("2. Посмотреть историю вычеслений ");
+        System.out.println("3. Выйти ");
+    }
+
+}
